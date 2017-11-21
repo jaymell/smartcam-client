838b452a84315d9679aea1a4d4ce1fc3686da64a
James Lucas <jamesmlucas@gmail.com>
2017-11-21 00:25:07 -0600

    add more to layoutContainer

--- src/LayoutContainer.js
+++ src/LayoutContainer.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const API = 'http://localhost:8080';
 
 class LayoutContainer extends Component {
+  constructor() {
+    this.api = API;
+  }
+  async componentDidMount() {
+    const cameras = await axios.get("${this.api}/cameras");
+    this.setState({ cameras: cameras });
+  }
 
+  render() {
+    return (
+      <CameraList cameras={this.state.cameras} />
+    );
+  }
 }
 
 export default LayoutContainer;
